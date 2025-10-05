214b72c9326071b30dfbe6e97bafce3362b86584
ktbm <boliventtalla12@gmail.com>
2025-10-05 03:52:03 +0100

    modification du templates

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -177,3 +177,13 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
 }
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
+}
