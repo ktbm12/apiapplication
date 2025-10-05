5171ac9486948e591124bbe1752b6cb60830a1df
ktbm <boliventtalla12@gmail.com>
2025-10-05 03:12:05 +0100

    modification du templates

--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ def home(request):
     """
     Vue minimale pour afficher la page d'accueil.
     """
-    return render(request, "pages/contact.html")
+    return render(request, "pages/comptes.html")
 
 
 def login(request):
